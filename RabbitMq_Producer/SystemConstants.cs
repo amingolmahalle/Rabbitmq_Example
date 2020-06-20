863a496a3306421d6f013526a88931e5234be356
Amin <amin.golmahalle@gmail.com>
2020-06-21 00:41:54 +0430

    changes in appsettings and SystemConstants

--- RabbitMq_Producer/SystemConstants.cs
+++ RabbitMq_Producer/SystemConstants.cs
@@ -3,6 +3,6 @@ namespace RabbitMq_Producer
     public static class SystemConstants
     {
         public const string HostEndpointId = "ba51ac1b-d974-4722-a639-a967514478d8";
-        public const string HostEndpointName = "Producer.Host";
+        public const string HostEndpointName = "RabbitMq_Producer";
     }
 }
